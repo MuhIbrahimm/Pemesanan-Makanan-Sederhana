13d66563a8b8c3c6ca7b4e7daa38d85bd26b724c
MuhIbrahimm <baim21121994@gmail.com>
2023-04-04 16:22:15 +0800

    login sederhana untuk masing masing user

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\n\n========= Selamat Datang =========");
+        System.out.println("=== Aplikasi Pemesanan Makanan ===");
+        login();
+    }
+
+    public static void login() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nUsername    : ");
+        String username = scanner.nextLine();
+        System.out.print("Password    : ");
+        String password = scanner.nextLine();
+
+        if (username.equals("admin") && password.equals("admin")) {
+            //perintah masuk ke sisi admin
+            System.out.println("anda admin");
+        } else if (username.equals("customer") && password.equals("customer")) {
+            System.out.println("anda customer");
+            //perintah masuk ke sisi admin
+        } else {
+            System.out.println("\nUsername dan Password tidak sesuai");
+            System.out.println("Tolong masukkan Username dan Password kembali");
+            login();
+            //perulangan login apabila username dan password tidak sesuai
+        }
     }
 }
